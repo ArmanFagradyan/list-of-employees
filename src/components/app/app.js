a029acb162ce73c10ad46f0bf90ccab161a91de7
ArmanFagradyan <63006219+ArmanFagradyan@users.noreply.github.com>
2023-02-26 19:20:51 +0400

    Search&Filter/Final

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,9 @@ class App extends Component {
                 {name: 'A. Smith', salary: 800, increase: false, rise: true, id: 1},
                 {name: 'D. Draper', salary: 1500, increase: true, rise: false, id: 2},
                 {name: 'H. Spectre', salary: 12300, increase: true, rise: false, id: 3}
-            ]
+            ],
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -102,23 +104,54 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'promo':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter});
+    }
+
     render() {
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter);
         return (
             <div className="app">
                 <AppInfo employees={employees} increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
-                    <AppFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
                 
                 <EmployeesList 
-                data={this.state.data}
-                onDelete={this.deleteItem}
-                onToggleProp={this.onToggleProp}
-                // onToggleRise={this.onToggleRise}
+                    data={visibleData}
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}
+                    // onToggleRise={this.onToggleRise}
                 />
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
